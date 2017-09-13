1c8bccec2cb67b94d079338d80b56aff72f129b0
SergeiKaptelin <serhii.kaptelin@gmail.com>
2017-09-13 13:17:00 +0300

    added Search Bar

--- Projects/ReactJS/ReduxSimpleSrarter/src/index.js
+++ Projects/ReactJS/ReduxSimpleSrarter/src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyANvDCNYVRM3vP5fiJ3ZHxK1gxbR0tKxTI';
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
