7289d8bb28bf3ac5f48d5beabf3a969ed142bd15
SergeiKaptelin <serhii.kaptelin@gmail.com>
2017-09-14 13:35:56 +0300

    added api search for youtube videos and made App class component

--- Projects/ReactJS/ReduxSimpleSrarter/src/index.js
+++ Projects/ReactJS/ReduxSimpleSrarter/src/index.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyANvDCNYVRM3vP5fiJ3ZHxK1gxbR0tKxTI';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component{
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'xiaomi'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
