36ac4ff81cd21b3b437667e3f79433610a219913
SergeiKaptelin <serhii.kaptelin@gmail.com>
2017-09-14 15:11:13 +0300

    adde video list and video list item to app

--- Projects/ReactJS/ReduxSimpleSrarter/src/index.js
+++ Projects/ReactJS/ReduxSimpleSrarter/src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyANvDCNYVRM3vP5fiJ3ZHxK1gxbR0tKxTI';
 
@@ -20,6 +21,7 @@ class App extends Component{
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos}/>
       </div>
     );
   }
