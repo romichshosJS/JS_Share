70d63e743db7d1207edc28a9d78bd27c41d7b4a5
SergeiKaptelin <serhii.kaptelin@gmail.com>
2017-09-16 14:22:51 +0300

    added video from you tube, made click to video list item work

--- Projects/ReactJS/ReduxSimpleSrarter/src/index.js
+++ Projects/ReactJS/ReduxSimpleSrarter/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyANvDCNYVRM3vP5fiJ3ZHxK1gxbR0tKxTI';
 
@@ -10,10 +11,16 @@ class App extends Component{
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-    YTSearch({key: API_KEY, term: 'xiaomi'}, (videos) => {
-      this.setState({ videos });
+    YTSearch({key: API_KEY, term: 'react vs vue'}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
@@ -21,7 +28,10 @@ class App extends Component{
     return (
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos}/>
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos} />
       </div>
     );
   }
