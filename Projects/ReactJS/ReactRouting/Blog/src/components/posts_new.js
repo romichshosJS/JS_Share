ac003709ef673ca015441c6bd18be533b3fed2dd
SergeiKaptelin <serhii.kaptelin@gmail.com>
2017-09-27 19:55:56 +0300

    added create post view

--- Projects/ReactJS/ReactRouting/Blog/src/components/posts_new.js
+++ Projects/ReactJS/ReactRouting/Blog/src/components/posts_new.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index.js';
 
 class PostsNew extends Component {
   render() {
+    const { fields: { title, categories, content }, handleSubmit } = this.props;
+    console.log(title);
+
     return (
-      <div>Create form</div>
+      <form onSubmit={handleSubmit(this.props.createPost)}>
+        <h3>Create A New Post</h3>
+        <div className="form-group">
+          <label>Title</label>
+          <input type="text" className="form-control" {...title} />
+        </div>
+
+        <div className="form-group">
+          <label>Categories</label>
+          <input type="text" className="form-control" {...categories} />
+        </div>
+
+        <div className="form-group">
+          <label>Content</label>
+          <textarea className="form-control" {...content} />
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     );
   }
 }
 
-export default PostsNew;
+//reduxForm: 1st is form config, 2nd is mapStateToProps, 3rd is mapDispatchToProps
+
+export default reduxForm({
+  form: 'PostsNewForm',
+  fields: ['title', 'categories', 'content']
+}, null, { createPost })(PostsNew);
