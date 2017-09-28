0d4cf0c256f8ef4bb2cc10eb18d1f41433fae4fc
SergeiKaptelin <serhii.kaptelin@gmail.com>
2017-09-28 20:01:01 +0300

    added article

---
+++ Projects/ReactJS/js_ru/src/Article.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+export default class Article extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isOpen: false
+    }
+  }
+
+  render() {
+    const {article} = this.props
+    const {isOpen} = this.state
+    return (
+      <div>
+        <h3>{article.title}</h3>
+        <button onClick = {this.toggleOpen}>
+          {isOpen ? 'close' : 'open'}
+        </button>
+        {this.getBody()}
+      </div>
+    )
+  }
+
+  getBody() {
+    console.log(this.state.isOpen);
+    if (!this.state.isOpen) return null
+    const {article} = this.props
+    return <section>{article.text}</section>
+  }
+
+  toggleOpen = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
+}
